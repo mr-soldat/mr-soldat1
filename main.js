5442d1ac9749f0e0b71f00a1f1fee4be363e2122
mr-soldat <56586998+mr-soldat@users.noreply.github.com>
2021-06-16 16:11:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,25 @@ document.addEventListener("DOMContentLoaded", () => {
             taskList.tasks.remove(this);
             taskList.closeEdit();
         };
-   
+    }
+
+    const vm = function() {
+        this.tasks = ko.observableArray([]);
+        this.tasksTODO = ko.computed(() => this.tasks().filter(x => x.status() === statusTODO));
+        this.tasksInWork = ko.computed(() => this.tasks().filter(x => x.status() === statusInWork));
+        this.tasksComplite = ko.computed(() => this.tasks().filter(x => x.status() === statusComplite));
+        this.sumTODO = ko.computed(() => this.tasksTODO().reduce((sum, x) => sum + ~~x.amount(), 0));
+        this.sumInWork = ko.computed(() => this.tasksInWork().reduce((sum, x) => sum + ~~x.amount(), 0));
+        this.sumComplite = ko.computed(() => this.tasksComplite().reduce((sum, x) => sum + ~~x.amount(), 0));
+        this.taskEdit = ko.observable();
+        this.newTask = () => {
+            this.taskEdit(new task());
+            this.tasks.push(this.taskEdit());
+        }
+        this.closeEdit = () => { this.taskEdit(undefined); }
+
+    };
+
     taskList = new vm();
     ko.applyBindings(taskList);
     taskList.tasks(dataTasks.map(x => new task(x)));
